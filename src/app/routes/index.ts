767659d2f6a24f9f8563bc5b12aeb3d855906c3a
shahidmonowarr <shahidmonowar21@gmail.com>
2023-11-05 21:51:54 +0600

    Backend Setup using moduler pattern

---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  // ... routes
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
